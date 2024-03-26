9649e6580a3f8ad52b0344c22438e5cf757f7ff3
egovos <60936636+egovos@users.noreply.github.com>
2024-03-26 10:15:14 +0900

    Create abc.c

---
+++ Source/mar19/abc.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int cnt;
+
+    printf("알파벳 소문자가 출력됩니다.\n");
+
+    // for (cnt = 65; cnt <= 90; cnt++) {
+    //     printf(" %c", cnt);
+    // }
+
+    // printf("\n");
+
+    for (cnt = 97; cnt <= 122; cnt++)
+    {
+        printf(" %c", cnt);
+    }
+
+    return 0;
+}
