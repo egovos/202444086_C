36d6f11d227f5a736a1c6b8f96c22760201640c0
egovos <60936636+egovos@users.noreply.github.com>
2024-03-26 10:41:22 +0900

    Create 2.c

---
+++ Source/mar26/2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int i = 1;
+
+    while (i <= 5) {
+        i++;
+
+        printf("%d", i);
+    }
+    while ('a') {
+        i++;
+
+        printf("%d", i);
+    }
+  
+
+    return 0;
+}
