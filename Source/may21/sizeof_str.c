8ae4fbe274f1eb4cf2c65e0dcef78b3ef08b7a22
にゆたみ <60936636+egovos@users.noreply.github.com>
2024-05-21 10:51:55 +0900

    Create sizeof_str.c

---
+++ Source/may21/sizeof_str.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    char str[20];
+    int length = 0;
+
+    printf("문> 문자열을 20바이트 이내로 입력하시오: ");
+    scanf("%s", str);
+    printf("\n");
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        length++;
+    }
+
+    printf("답> 입력한 문자열의 길이: %d바이트\n", length);
+
+    return 0;
+}
