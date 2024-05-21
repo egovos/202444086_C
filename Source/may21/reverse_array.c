3d7daf01e4b2c0dece5adcaee2a765aff74083a4
にゆたみ <60936636+egovos@users.noreply.github.com>
2024-05-21 10:35:03 +0900

    Update reverse_array.c

--- Source/may21/reverse_array.c
+++ Source/may21/reverse_array.c
@@ -1,29 +1,29 @@
-#include <stdio.h>
+    #include <stdio.h>
 
-int main() {
-    int size, maxheight, cnt = 0;
+    int main() {
+        int size, maxheight, cnt = 1;
 
-    scanf("%d", &size);
+        scanf("%d", &size);
 
-    int arr[size];
+        int arr[size];
 
-    for (int i = 0; i < size; i++) {
-        scanf("%d", &arr[i]);
-    }
-
-    maxheight = -1;
-
-    for (int i = size - 1; i > 0; i--) {
-        if (arr[i] > maxheight) {
-            printf("%d > %d | %d\n", arr[i], maxheight, i);
-
-            maxheight = arr[i];
-
-            cnt++;
+        for (int i = 0; i < size; i++) {
+            scanf("%d", &arr[i]);
         }
+
+        maxheight = arr[size - 1];
+
+        for (int i = size - 2; i > 0; i--) {
+            if (arr[i] > maxheight) {
+                printf("%d > %d | %d\n", arr[i], maxheight, i);
+
+                maxheight = arr[i];
+
+                cnt++;
+            }
+        }
+
+        printf("%d\n", cnt);
+
+        return 0;
     }
-
-    printf("%d\n", cnt);
-
-    return 0;
-}
